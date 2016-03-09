62000c60cc2298f6c6910122201412379db10381
Nina <ninaolo@kth.se>
2016-03-09 20:37:49 +0100

    Add truststore

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class Server {
 
 	public static void main(String[] args) {
+
+        System.setProperty("javax.net.ssl.keyStore","/Users/Nina/git/intnet-lab6/lab6keystore");
+        System.setProperty("javax.net.ssl.trustStore","/Users/Nina/git/intnet-lab6/lab6truststore");
+        System.setProperty("javax.net.ssl.keyStorePassword","ninaeric");
+        System.setProperty("javax.net.ssl.trustStorePassword","ninaeric");
 
 		SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
 		System.out.println("Stöder:");
@@ -16,22 +20,26 @@ public class Server {
 		for (int i = 0; i < ssf.getSupportedCipherSuites().length; i++)
 			System.out.println(ssf.getSupportedCipherSuites()[i]);
 
-		SSLServerSocket ss = null;
+		SSLServerSocket serverSocket = null;
 		try {
-			ss = (SSLServerSocket) ssf.createServerSocket(1234);
+			serverSocket = (SSLServerSocket) ssf.createServerSocket(1234);
 			String[] cipher = { "SSL_DH_anon_WITH_RC4_128_MD5" };
-			ss.setEnabledCipherSuites(cipher);
+			serverSocket.setEnabledCipherSuites(cipher);
 			System.out.println("Vald:");
 
-			for (int i = 0; i < ss.getEnabledCipherSuites().length; i++)
-				System.out.println(ss.getEnabledCipherSuites()[i]);
+			for (int i = 0; i < serverSocket.getEnabledCipherSuites().length; i++)
+				System.out.println(serverSocket.getEnabledCipherSuites()[i]);
 
-			SSLSocket s = (SSLSocket) ss.accept();
-			BufferedReader infil = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			String rad = null;
-			while ((rad = infil.readLine()) != null)
-				System.out.println(rad);
-			infil.close();
+            // Listen for connections, send them a message and close them down
+			while(true) {
+                System.out.println("Listening for connections...");
+				SSLSocket socket = (SSLSocket) serverSocket.accept();
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                out.println("Hello World!");
+                out.flush();
+                socket.close();
+			}
+
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
