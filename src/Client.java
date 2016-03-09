ad58c2012a3e54b2b7f4ff45849ea092c2f2b3bd
Nina <ninaolo@kth.se>
2016-03-09 20:02:06 +0100

    Add keystore + cer

--- src/Client.java
+++ src/Client.java
@@ -9,23 +9,26 @@ import javax.net.ssl.SSLSocketFactory;
 public class Client {
 	public static void main(String[] args) {
 		SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
-
+/*
 		for (int i = 0; i < sf.getSupportedCipherSuites().length; i++)
 			System.out.println(sf.getSupportedCipherSuites()[i]);
-
+*/
 		HttpsURLConnection.setDefaultSSLSocketFactory(sf);
 		SSLSocket s = null;
 		try {
-			s = (SSLSocket) sf.createSocket("my.nada.kth.se", 1234);
+			s = (SSLSocket) sf.createSocket("127.0.0.1", 1234);
 		} catch (MalformedURLException e) {
 			System.out.println(e.getMessage());
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-		for (int i = 0; i < s.getSupportedCipherSuites().length; i++)
+		System.out.println("SERVER'S SUPPORTED CIPHERS");
+		for (int i = 0; i < s.getSupportedCipherSuites().length; i++) {
 			System.out.println(s.getSupportedCipherSuites()[i]);
+		}
 		String[] cipher = { "SSL_DH_anon_WITH_RC4_128_MD5" };
 		s.setEnabledCipherSuites(cipher);
+		System.out.println("SERVER'S ENABLED CIPHERS");
 		for (int i = 0; i < s.getEnabledCipherSuites().length; i++)
 			System.out.println(s.getEnabledCipherSuites()[i]);
 		PrintWriter utfil = null;
